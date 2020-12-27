234a4a9d69ffdb79cd14137b23a6b8f03a220d87
Bruno Garcia <bgarciamoura@gmail.com>
2020-12-26 23:36:56 -0300

    Pokedex Kanto

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,60 @@
-import styled from 'styled-components'
+import { GetStaticProps } from 'next';
+import React from 'react';
+import styled from 'styled-components';
+
+/**
+ * LINK DA API PARA IMAGENS
+ * https://pokeapi.co/api/v2/pokemon/1/
+ * substituir o id no fim
+ * seção sprites do json
+ */
+
+export const getStaticProps: GetStaticProps = async () => {
+    const pokemons = await fetch('https://pokeapi.co/api/v2/pokedex/2/')
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+        })
+        .then((JSONresponse) => {
+            return JSONresponse.pokemon_entries;
+        });
+
+    return {
+        props: {
+            pokemons,
+        },
+    };
+};
+
+interface Pokemon {
+    entry_number: number;
+    pokemon_species: {
+        name: string;
+        url: string;
+    };
+}
+
+const Home: React.FC<{ pokemons: Pokemon[] }> = ({ pokemons }) => {
+    return (
+        <>
+            <div>
+                <ul>
+                    {pokemons.map((pokemon: Pokemon) => (
+                        <li key={pokemon.entry_number}>
+                            <Title>{pokemon.pokemon_species.name}</Title>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    );
+};
 
 const Title = styled.h1`
-  color: red;
-  font-size: 50px;
-`
+    color: red;
+    font-size: 16px;
+    font-family: sans-serif;
+`;
 
-export default function Home() {
-  return <Title>My page</Title>
-}
+export default Home;
