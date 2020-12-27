83ce502a2621a1376f12b82831f2e5523116d1f7
Bruno Garcia <bgarciamoura@gmail.com>
2020-12-27 15:49:56 -0300

    Altered the next.config file

--- next.config.js
+++ next.config.js
@@ -4,4 +4,7 @@ module.exports = {
         base_api_url: 'http://localhost:3000/api',
         base_pokemon_url: 'https://pokeapi.co/api/v2',
     },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
 };
